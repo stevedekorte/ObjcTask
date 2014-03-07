aada35ccb533f8d4e232b7bea63c4373ad202c36
Steve Dekorte <steve@dekorte.com>
2014-03-06 16:47:07 -0800

    cleanup

--- Task.h
+++ Task.h
@@ -10,13 +10,13 @@
 @interface Task : NSObject
 {
     char **_env;
-	char **_args;
-	callsystem_fd_t _stdin_child[2];
-	callsystem_fd_t _stdout_child[2];
-	callsystem_fd_t _stderr_child[2];
-	callsystem_pid_t _pid;
-	int _status;
-	int _needsClose;
+    char **_args;
+    callsystem_fd_t _stdin_child[2];
+    callsystem_fd_t _stdout_child[2];
+    callsystem_fd_t _stderr_child[2];
+    callsystem_pid_t _pid;
+    int _status;
+    int _needsClose;
 }
 
 @property (strong, nonatomic) NSString *launchPath;
@@ -27,14 +27,10 @@
 @property (strong, nonatomic) NSFileHandle *standardOutput;
 @property (strong, nonatomic) NSFileHandle *standardError;
 
-//@property (assign) int terminationStatus;
-
 - (int)processIdentifier;
-
 - (void)launch;
 - (void)terminate;
 - (void)waitUntilExit;
 - (BOOL)isRunning;
-
 
 @end
